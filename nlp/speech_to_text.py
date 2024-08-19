d2ee405491797267dabc9b5d956eb91be321dfc2
Ryan Chung <chungjr@umich.edu>
2024-08-19 18:27:19 -0400

    updated comment

--- nlp/speech_to_text.py
+++ nlp/speech_to_text.py
@@ -6,7 +6,7 @@ def stt(filepath: Path) -> str:
     with sr.AudioFile(str(filepath)) as source:
         audio = r.record(source)
 
-    # this uses google speech recognition
+    # this uses google speech recognition, there are a bunch of other models available in this library
     try:
         speech = r.recognize_google(audio)
     except sr.UnknownValueError:
