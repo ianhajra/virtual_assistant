01af6db6a62dfe50989c39c98680c533dbcc294d
Ian Hajra <ian_hajra@brown.edu>
2024-08-19 22:54:22 -0400

    #35 Progress

--- core/src/engine.cxx
+++ core/src/engine.cxx
@@ -4,4 +4,32 @@
 #include "../include/module_manager.h"
 #include "../include/utils.h"
 
-// TODO: Include Implementations Below
+/**
+ * This constructor will initialize all of the appropriate classes
+ * and will then proceed to begin running
+ */
+Engine::Engine()
+{
+    this->init();
+    this->run();
+}
+
+Engine::~Engine()
+{
+    // empty destructor for now
+}
+
+/**
+ * Function used to initialize the engine. It will create the following:
+ *   - utils
+ *   - a logger
+ *   - a config_loader
+ *   - a module_manaer
+ */
+void Engine::init()
+{
+}
+
+void Engine::run()
+{
+}
