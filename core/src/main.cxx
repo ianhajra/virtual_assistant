4f9950d1f45a9aa24c921821567b486de7bd8e7f
Ryan Chung <chungjr@umich.edu>
2024-08-19 22:25:16 -0400

    hello world

--- core/src/main.cxx
+++ core/src/main.cxx
@@ -3,5 +3,10 @@
 #include "../include/logger.h"
 #include "../include/module_manager.h"
 #include "../include/utils.h"
+#include <iostream>
 
 // TODO: Write Code Here
+int main(int argc, char* argv[]){
+    std::cout << "Hello World!\n";
+
+}
