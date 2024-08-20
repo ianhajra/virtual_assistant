7286ecf169c9a5ca1e88677fb3e3be53dc0e6207
Ian Hajra <ian_hajra@brown.edu>
2024-08-20 11:44:09 -0400

    #38: Header Initial Implementation

--- core/include/logger.h
+++ core/include/logger.h
@@ -1,11 +1,35 @@
 #pragma once
 
+#include <string>
+#include <fstream>
+
+/**
+ * LogLevel class used to determine what type of logging to do
+ */
+enum class LogLevel {
+    DEBUG,
+    WARNING,
+    ERROR
+};
+
+/**
+ * Updated Logger class to handle the appropriate methods that will be used
+ * for logging throughout the virtual_assistant
+ */
 class Logger
 {
 public:
-    Logger();
-
+    Logger(const std::string& filename);
     ~Logger();
 
+    void log(LogLevel level, const std::string& message);
+
+    void logDebug(const std::string& message);
+    void logWarning(const std::string& message);
+    void logError(const std::string& message);
+
 private:
+    std::ofstream logFile;
+    std::string levelToString(LogLevel level);
+    void logMessage(const std::string& levelStr, const std::string& message);
 };
