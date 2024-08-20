869b7f39cca9eaa47c89ecc0d4864d2924647d15
Ian Hajra <ian_hajra@brown.edu>
2024-08-20 13:15:54 -0400

    44: Having Main instantiate an engine

--- core/src/main.cxx
+++ core/src/main.cxx
@@ -1,8 +1,7 @@
 #include "../include/engine.h"
 #include <iostream>
 
-// TODO: Write Code Here
 int main(int argc, char* argv[]){
-    std::cout << "Hello World!\n";
+    Engine engine = Engine();
     return 0;
 }
